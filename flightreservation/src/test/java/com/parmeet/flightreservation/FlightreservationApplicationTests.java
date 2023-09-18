931d9824493f104887cf3334149fef0c95234a29
Parmeet Singh <psparmeet14@gmail.com>
2023-09-18 21:30:17 +0530

    feat: added flight reservation project

---
+++ flightreservation/src/test/java/com/parmeet/flightreservation/FlightreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parmeet.flightreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
