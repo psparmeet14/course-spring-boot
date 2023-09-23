156420aa86a759f05c7c221d94cd6c9d8b414554
Parmeet Singh <psparmeet14@gmail.com>
2023-09-23 10:35:33 +0530

    feat: handled login functionality in flightreservation

--- flightreservation/src/main/java/com/parmeet/flightreservation/controllers/UserController.java
+++ flightreservation/src/main/java/com/parmeet/flightreservation/controllers/UserController.java
@@ -4,9 +4,11 @@ import com.parmeet.flightreservation.entities.User;
 import com.parmeet.flightreservation.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -23,5 +25,18 @@ public class UserController {
     public String registerUser(@ModelAttribute("user") User user) {
         userRepository.save(user);
         return "login/login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestParam("email") String email, @RequestParam("password") String password,
+                        ModelMap modelMap) {
+        User user = userRepository.findByEmail(email);
+        if (user.getPassword().equals(password)) {
+            return "findFlights";
+        } else {
+            modelMap.addAttribute("msg", "Invalid username or password. Please try again!");
+        }
+        return "login/login";
+
     }
 }
