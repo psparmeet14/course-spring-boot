7a9c72a34e5ccd1dcfb20d7da9f13aa4117e6399
Parmeet Singh <psparmeet14@gmail.com>
2023-10-08 12:49:59 +0530

    feat: updated readme, added spring security dependency, encrypter password

--- flightreservation/src/main/java/com/parmeet/flightreservation/controllers/UserController.java
+++ flightreservation/src/main/java/com/parmeet/flightreservation/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.parmeet.flightreservation.repos.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,6 +18,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
@@ -35,13 +39,17 @@ public class UserController {
     @RequestMapping(value = "/registerUser", method = RequestMethod.POST)
     public String registerUser(@ModelAttribute("user") User user) {
         LOGGER.info("Inside registerUser() " + user);
+        user.setPassword(encoder.encode(user.getPassword()));
         userRepository.save(user);
         return "login/login";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@RequestParam("email") String email, @RequestParam("password") String password,
-                        ModelMap modelMap) {
+    public String login(
+            @RequestParam("email") String email,
+            @RequestParam("password") String password,
+            ModelMap modelMap
+    ) {
         LOGGER.info("Inside login() and the email is: " + email);
 
         User user = userRepository.findByEmail(email);
